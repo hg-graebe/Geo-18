b537acfea6cb714015ac3cc819c3d9d36bcbf939
kev007 <kev_shrestha@hotmail.com>
2018-04-14 12:48:06 +0200

    jsxgraph test

--- Gruppe-2/src/helperFunctions.py
+++ Gruppe-2/src/helperFunctions.py
@@ -2,6 +2,7 @@ import zipfile
 
 
 def unzip(path, destinationFolder):
+    temp = path.split("/")
     zip_ref = zipfile.ZipFile(path, 'r')
-    zip_ref.extractall(destinationFolder)
+    zip_ref.extractall(destinationFolder + "/" + temp[len(temp)-1])
     zip_ref.close()
