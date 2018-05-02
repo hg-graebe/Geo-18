9e7f39bbfc8a5f0dd4c8af833deb6fc9cb47ee52
Hannes Thalheim <pge10btu@studserv.uni-leipzig.de>
2018-05-02 17:52:44 +0200

    [Gruppe-4] Adjusted start_server.sh and testcases.html.
    start_server
    ============
    * now detects OS and echos it
    * now echos server starting command
    * running `http.server` on Windows
    
    testcases
    =========
    * Changed address `0.0.0.0` to `localhost` to work on every OS
    
    doc
    ===
    * Added whitepapers about intergeo- and i2g-format

--- Gruppe-4/start_server.sh
+++ Gruppe-4/start_server.sh
@@ -1,3 +1,18 @@
 #!/bin/bash
 
-python -m SimpleHTTPServer 8000
+UNAME=$(uname)
+
+if [ "$UNAME" == "Linux" ] ; then
+	echo "Linux"
+	echo "todo"
+elif [ "$UNAME" == "Darwin" ] ; then
+	echo "Mac"
+	echo "python -m SimpleHTTPServer 8000"
+	python -m SimpleHTTPServer 8000
+elif [[ "$UNAME" == CYGWIN* || "$UNAME" == MINGW* ]] ; then
+	echo "Windows"
+	echo "python -m http.server 8000"
+	python -m http.server 8000
+else
+	echo "Unknown OS"
+fi
