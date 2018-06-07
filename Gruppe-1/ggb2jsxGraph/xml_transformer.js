cd9c78834370282b5acbb5d02ea46c02584866c3
Tobias Mann <tm15ceqe@studserv.uni-leipzig.de>
2018-06-07 10:35:10 +0200

    Gruppe 1: Implement more tools

--- Gruppe-1/ggb2jsxGraph/xml_transformer.js
+++ Gruppe-1/ggb2jsxGraph/xml_transformer.js
@@ -12,11 +12,14 @@ const geogebraBoilerplate = `<?xml version="1.0" encoding="utf-8"?>
 </construction>
 </geogebra>`;
 
+const GGB_HIGHLIGHT_RED = '<objColor r="255" g="0" b="0" alpha="0.0"/>';
+const GGB_COORDS_SLIDER = '<coords x="1" y="1" z="1"/>';
+
 // to keep track of ids of construction tools used by macros
 // so we don't use them multiple times
-const macroIdRepository = {};
+let macroIdRepository = {};
 
-const propExplanations = [];
+let propExplanations = [];
 
 // construction tools that are entirely equivalent between Geoproof/GeoGebra, including parameters and their order
 const commandNameMapping = {
@@ -25,28 +28,68 @@ const commandNameMapping = {
     'midpoint': 'Midpoint',
     'pp_line': 'Line',
     'ortho_line': 'OrthogonalLine',
-    'par_line': 'Line'
+    'par_line': 'Line',
+    'pc_circle': 'Circle',
+    'p3_circle': 'Circle'
 };
 // any construction tools not covered by the above
 // callback arguments are always (...inputs, output)
 const commandMacroMapping = {
     'altitude': (pointTop, pointBase1, pointBase2, finalOutput) => {
         const lineId = `base of ${finalOutput}`;
-        const returnStr = createCommandXml('Line', [pointBase1, pointBase2], lineId, 'line') +
+        const returnStr =
+            createCommandXml('Line', [pointBase1, pointBase2], lineId, 'line') +
             createCommandXml('OrthogonalLine', [pointTop, lineId], finalOutput, 'line');
         markIdUsed(lineId);
         return returnStr;
+    },
+    'pedalpoint': (point, line, finalOutput) => {
+        const orthogonalLineId = `ortho(${point}, ${line})`;
+        const returnStr =
+            createCommandXml('OrthogonalLine', [point, line], orthogonalLineId, 'line') + 
+            createCommandXml('Intersect', [line, orthogonalLineId], finalOutput, 'point');
+        markIdUsed(orthogonalLineId);
+        return returnStr;
+    },
+    'median': (pointTop, pointBase1, pointBase2, finalOutput) => {
+        const midpointId = `mid(${pointBase1}, ${pointBase2})`;
+        const returnStr =
+            createCommandXml('Midpoint', [pointBase1, pointBase2], midpointId, 'point') +
+            createCommandXml('Line', [pointTop, midpointId], finalOutput, 'line');
+        markIdUsed(midpointId);
+        return returnStr;
+    },
+    'line_slider': (line, finalOutput) => {
+        return createCommandXml('Point', [line], finalOutput, 'point', [GGB_COORDS_SLIDER]);
     }
 };
 
 const propMacroMapping = {
-    'eq_dist': (firstLineFirstPoint, firstLineSecondPoint, secondLineFirstPoint, secondLineSecondPoint) => {
-        const firstLineId = `dist(${firstLineFirstPoint}, ${firstLineSecondPoint})`;
-        const secondLineId = `dist(${secondLineFirstPoint}, ${secondLineSecondPoint})`;
-        const returnStr = createCommandXml('Segment', [firstLineFirstPoint, firstLineSecondPoint], firstLineId, 'line', true) +
-            createCommandXml('Segment', [secondLineFirstPoint, secondLineSecondPoint], secondLineId, 'line', true);
+    'eq_dist': (A, B, C, D) => {
+        const firstLineId = `dist(${A}, ${B})`;
+        const secondLineId = `dist(${C}, ${D})`;
+        const returnStr =
+            createCommandXml('Segment', [A, B], firstLineId, 'line', [GGB_HIGHLIGHT_RED]) +
+            createCommandXml('Segment', [C, D], secondLineId, 'line', [GGB_HIGHLIGHT_RED]);
         markIdUsed(firstLineId, secondLineId);
-        propExplanations.push(`Distance (${firstLineFirstPoint}, ${firstLineSecondPoint}) is equal to distance (${secondLineFirstPoint}, ${secondLineSecondPoint})`)
+        propExplanations.push(`Distance (${A}, ${B}) is equal to distance (${C}, ${D})`)
+        return returnStr;
+    },
+    'eq_angle': (A, B, C, D, E, F) => {
+        const firstAngleId = `ang(${A}, ${B}, ${C})`;
+        const secondAngleId = `ang(${D}, ${E}, ${F})`;
+        const returnStr =
+            createCommandXml('Angle', [A, B, C], firstAngleId, 'angle', [GGB_HIGHLIGHT_RED]) +
+            createCommandXml('Angle', [D, E, F], secondAngleId, 'angle', [GGB_HIGHLIGHT_RED]);
+        markIdUsed(firstAngleId, secondAngleId);
+        propExplanations.push(`Angle (${A}, ${B}, ${C}) is equal to angle (${D}, ${E}, ${F})`)
+        return returnStr;
+    },
+    'is_concurrent': (...lines) => {
+        const intersectionId = `inter(${lines.join(', ')})`;
+        const returnStr = createCommandXml('Intersect', lines, intersectionId, 'point', [GGB_HIGHLIGHT_RED]);
+        markIdUsed(intersectionId);
+        propExplanations.push(`Lines ${lines.join(', ')} are concurrent`);
         return returnStr;
     }
 };
@@ -82,11 +125,12 @@ function transformXml(inputXml) {
 function* createCoordGenerator() {
     yield [0, 1];
     yield [1, 0];
-    yield [2, 0.5];
-    yield [0.7, 1.7];
+    yield [2, 1.5];
+    yield [0.7, 3];
+    yield [3, 2];
 }
 
-function createCommandXml(commandName, inputIds, outputId, outputType, highlight) {
+function createCommandXml(commandName, inputIds, outputId, outputType, additionalElementData) {
     // check if outputId has been used already, so we don't get duplicates
     if (macroIdRepository[outputId]) return '';
 
@@ -96,9 +140,9 @@ function createCommandXml(commandName, inputIds, outputId, outputType, highlight
             <input ${inputsIdsAsAttributes.join(' ')}/>
             <output a0="${outputId}"/>
         </command>
-        ${highlight ?
+        ${additionalElementData ?
             `<element type="${outputType}" label="${outputId}">
-                <objColor r="255" g="0" b="0" alpha="0.0"/>
+                ${additionalElementData.join('\n')}
             </element>` :
             `<element type="${outputType}" label="${outputId}"/>`}
         `
@@ -107,47 +151,68 @@ function createCommandXml(commandName, inputIds, outputId, outputType, highlight
 
 function transformFreePointXml(inputXmlElement, freePointGenerator) {
     // the first character in geoproof IDs is always a $, so cut that
-    const pointId = inputXmlElement.getAttribute('id').substr(1);
+    const pointId = cleanId(inputXmlElement.getAttribute('id'));
     const coords = freePointGenerator.next().value;
-    if (!coords) throw "Not enough free point coordinates available";
+    if (!coords) fatalError("Not enough free point coordinates available");
     return (
         `<element type="point" label="${pointId}">
             <coords x="${coords[0]}" y="${coords[1]}" z="1.0"/>
+            <objColor r="50" g="50" b="200" alpha="0.0"/>
         </element>`
     );
 }
 
 function transformCommandXml(inputXmlElement) {
     // the first character in geoproof IDs is always a $, so cut that
-    const outputId = inputXmlElement.getAttribute('id').substr(1);
+    const outputId = cleanId(inputXmlElement.getAttribute('id'));
     const outputType = inputXmlElement.nodeName.toLowerCase();
     const commandCallStr = inputXmlElement.textContent;
     if (!commandCallStr) return '';
-    const commandName = commandCallStr.substring(0, commandCallStr.indexOf('['));
-    const uncleanInputIds = commandCallStr.substring(commandCallStr.indexOf('[') + 1, commandCallStr.indexOf(']')).split(',');
-    const inputIds = uncleanInputIds.map(str => str.trim().substr(1));
-    console.log(`command ${commandName} with inputs ${inputIds} and output ${outputId}`);
-    const mappedCommandName = commandNameMapping[commandName];
+    const command = parseCommand(commandCallStr);
+    console.log(`command ${command.name} with inputs ${command.inputs} and output ${outputId}`);
+    const mappedCommandName = commandNameMapping[command.name];
     if (!mappedCommandName) {
-        if (commandName in commandMacroMapping) {
-            return commandMacroMapping[commandName](...inputIds, outputId);
+        if (command.name in commandMacroMapping) {
+            return commandMacroMapping[command.name](...command.inputs, outputId);
         }
-        throw "Unknown command " + commandName;
+        fatalError("Unknown command " + command.name);
     }
-    return createCommandXml(mappedCommandName, inputIds, outputId, outputType);
+    return createCommandXml(mappedCommandName, command.inputs, outputId, outputType);
 }
 
 function transformPropXml(inputXmlElement) {
     const commandCallStr = inputXmlElement.textContent;
     if (!commandCallStr) return '';
-    const commandName = commandCallStr.substring(0, commandCallStr.indexOf('['));
-    const uncleanInputIds = commandCallStr.substring(commandCallStr.indexOf('[') + 1, commandCallStr.indexOf(']')).split(',');
-    const inputIds = uncleanInputIds.map(str => str.trim().substr(1));
-    console.log(`prop ${commandName} with inputs ${inputIds}`);
-    if (commandName in propMacroMapping) {
-        return propMacroMapping[commandName](...inputIds);
+    const command = parseCommand(commandCallStr);
+    console.log(`prop ${command.name} with inputs ${command.inputs}`);
+    if (command.name in propMacroMapping) {
+        return propMacroMapping[command.name](...command.inputs);
     } else {
-        throw "Unknown prop " + commandName;
+        fatalError("Unknown prop " + command.name);
     }
     return '';
+}
+
+function fatalError(msg) {
+    alert(msg);
+    throw msg;
+}
+
+function resetData() {
+    macroIdRepository = {};
+    propExplanations = [];
+}
+
+// removes leading $
+// fixes subscript for multi-digit numbers (by putting underscores in front every digit after the first one)
+function cleanId(originalId) {
+    return originalId.substr(1).replace(/\d+/g, (match) => match.split('').join('_'));
+}
+
+function parseCommand(commandStr) {
+    const uncleanInputIds = commandStr.substring(commandStr.indexOf('[') + 1, commandStr.indexOf(']')).split(',');
+    return {
+        name: commandStr.substring(0, commandStr.indexOf('[')),
+        inputs: uncleanInputIds.map(str => str.trim()).filter(str => str.startsWith('$')).map(cleanId)
+    }
 }
